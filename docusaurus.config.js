c74b18e00ba9bfc41f757fe10c997ce8b4c2e0f7
Gaëtan Devaux <gaetan@passbolt.com>
2023-06-22 16:40:01 +0200

    PB-24779 - Update main content layout

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,8 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateTime: true,
+          showLastUpdateAuthor: false,
           // Please change this to your repo.
           // Remove this to remove the 'edit this page' links.
           editUrl:

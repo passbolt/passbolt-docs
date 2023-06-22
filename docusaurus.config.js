121c3766f93f50a6a3d4e5c197f5055e710e786f
Gaëtan Devaux <gaetan@passbolt.com>
2023-06-22 09:31:33 +0200

    PB-24779 - Update OG image and favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/og.jpg',
       docs: {
         sidebar: {
           autoCollapseCategories: true,

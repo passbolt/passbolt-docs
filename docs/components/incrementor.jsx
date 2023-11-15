5588a8e38e76f36df2b2ec485f9e1202ec376253
Antony Bartolomucci <antony@passbolt.com>
2023-11-15 09:06:58 +0100

    PB-28642 - Upgrade Docusaurus to version 3

---
+++ docs/components/incrementor.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+let counter = 0;
+
+const Incrementor = ({ title, productName }) => {
+    counter += 1;
+
+    if (typeof title !== 'string') {
+        console.error('Incrementor expects a string as title');
+        return null; // ou retournez une valeur par défaut
+    }
+
+    const processedTitle = productName
+        ? title.replace(/\{productName\}/g, productName.toLowerCase())
+        : title;
+
+    return <b>Step {counter}. {processedTitle}</b>;
+};
+
+export default Incrementor;

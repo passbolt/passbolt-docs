d606a25183fd250dfc71877c8a52f7962a0021e5
pierre <pierre@passbolt.com>
2024-03-01 13:17:49 +0100

    PB-30096 - Fix copy to clipboard button

--- src/components/CodeBlock/CodeBlock.js
+++ src/components/CodeBlock/CodeBlock.js
@@ -1,27 +1,22 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Prism from 'prismjs';
+import CodeBlock from '@theme/CodeBlock';
 
-const CodeBlock = ({ children }) => {
-  useEffect(() => {
-    // This will highlight the code block after the component mounts
-    Prism.highlightAll();
-  }, []); // Empty dependency array means this runs once on mount
-
+const CodeBlockComponent = ({ children }) => {
   if (!children) return null;
 
   return (
-    <pre className="language-bash">
-      <code>{children}</code>
-    </pre>
+    <CodeBlock className="bash">
+      {children}
+    </CodeBlock>
   );
 };
 
-CodeBlock.propTypes = {
+CodeBlockComponent.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]).isRequired,
 };
 
-export default CodeBlock;
+export default CodeBlockComponent;

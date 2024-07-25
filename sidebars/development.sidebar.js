bc73a017d70ee9fbdb91be63c0db8b910155fdaf
Louis Vallat <louis@passbolt.com>
2024-07-25 13:17:32 +0200

    PB-34113 - Initial commit for openapi integration using redocusaurus with resources and resource types at first
    Signed-off-by: Louis Vallat <louis@passbolt.com>

---
+++ sidebars/development.sidebar.js
@@ -0,0 +1,18 @@
+// @ts-nocheck
+
+/**
+ * @type {import('@docusaurus/plugin-content-docs/src/sidebars/types').SidebarConfig}
+ */
+const developmentSidebar = [
+  {
+    type: 'doc',
+    id: 'development/index',
+    label: 'Introduction',
+  },
+  {
+    type: 'link',
+    href: 'api',
+    label: 'API Documentation',
+  },
+];
+module.exports = developmentSidebar;
